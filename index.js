20ea8ffb117f59028fbb3c77dff48b834b6fadfd
KhadizaSamiha <khadizasamiha7@gmail.com>
2023-06-09 20:54:49 +0600

    admin role patch api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
-// var jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -12,7 +12,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rfvro52.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,11 +31,13 @@ async function run() {
         const classesCollection = client.db("eliteDb").collection("classes");
         const usersCollection = client.db("eliteDb").collection("users")
 
+
         // class related apis
         app.get('/classes', async (req, res) => {
             const result = await classesCollection.find().toArray();
             res.send(result);
         })
+
         // users related apis
         app.post('/users', async(req, res) =>{
             const user = req.body;
@@ -45,6 +47,21 @@ async function run() {
                 return res.send({message : "user already exist"})
             }
             const result = await usersCollection.insertOne(user);
+            res.send(result)
+        })
+        app.get('/users', async(req, res) =>{
+            const result = await usersCollection.find().toArray();
+            res.send(result);
+        })
+        app.patch('/users/admin/:id', async(req, res) =>{
+            const id = req.params.id;
+            const filter = {_id : new ObjectId(id)}
+            const updateDoc = {
+                $set : {
+                    role : "admin"
+                },
+            }
+            const result = await usersCollection.updateOne(filter, updateDoc);
             res.send(result)
         })
 
