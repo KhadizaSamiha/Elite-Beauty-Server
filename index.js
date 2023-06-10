f2c79687ff8517415feca85da084655bf65d34e7
KhadizaSamiha <khadizasamiha7@gmail.com>
2023-06-10 12:32:37 +0600

    jwt impliment

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
-const jwt = require('jsonwebtoken');
-const port = process.env.PORT || 5000;
+var jwt = require('jsonwebtoken');
 require('dotenv').config();
+
+const port = process.env.PORT || 5000;
 
 
 // middleWare
 app.use(cors())
 app.use(express.json())
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
 
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rfvro52.mongodb.net/?retryWrites=true&w=majority`;
@@ -31,34 +47,48 @@ async function run() {
         const classesCollection = client.db("eliteDb").collection("classes");
         const usersCollection = client.db("eliteDb").collection("users")
 
+        // jwt api
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' })
+
+            res.send({ token })
+        })
 
         // class related apis
         app.get('/classes', async (req, res) => {
-            const result = await classesCollection.find().toArray();
+            const result = await classesCollection.find({status : 'approved'}).toArray();
             res.send(result);
         })
 
         // users related apis
-        app.post('/users', async(req, res) =>{
+        app.post('/users', verifyJWT, async (req, res) => {
             const user = req.body;
-            const query = {email : user.email}
+
+            // verify jwt 
+            const decodedEmail = req.decoded.email;
+            if (user.email !== decodedEmail) {
+                return res.status(403).send({ error: true, message: 'forbidden access' })
+            }
+
+            const query = { email: user.email }
             const existingUser = await usersCollection.findOne(query)
-            if(existingUser){
-                return res.send({message : "user already exist"})
+            if (existingUser) {
+                return res.send({ message: "user already exist" })
             }
             const result = await usersCollection.insertOne(user);
             res.send(result)
         })
-        app.get('/users', async(req, res) =>{
+        app.get('/users', async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
         })
-        app.patch('/users/admin/:id', async(req, res) =>{
+        app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id : new ObjectId(id)}
+            const filter = { _id: new ObjectId(id) }
             const updateDoc = {
-                $set : {
-                    role : "admin"
+                $set: {
+                    role: "admin"
                 },
             }
             const result = await usersCollection.updateOne(filter, updateDoc);
